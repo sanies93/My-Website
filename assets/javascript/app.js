acf941e2fde582adb371844b04c6d1dad6419f6c
Sahra Anies <sahra.anies@gmail.com>
2019-06-18 00:05:39 -0700

    initial development

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, options);
+// });
+
+// $(document).ready(function () {
+//     $('.sidenav').sidenav();
+// });
+
+// SideNav Button Initialization
+// $(".button-collapse").sideNav();
+
+// // SideNav Scrollbar Initialization
+// var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+// var ps = new PerfectScrollbar(sideNavScrollbar);
